0a035782a053eb33cfd0f163e748a9262654f1c3
gwgwhc <georgehudsonchang@gmail.com>
2021-11-19 14:32:11 +0100

    first working build

--- canvas.py
+++ canvas.py
@@ -3,14 +3,16 @@ from ROOT import *
 
 class Canvas():
     def __init__(self):
-
         # (eventually will be input)
         self.freq_min = 10
         self.freq_max = 280
 
         self.create_canvas()
-        self.create_histograms(self.freq_min, self.freq_max)
+        self.create_histograms()
         self.histogram_fill()
+
+        # prevents gui closing in pyroot. must go last in init!
+        gApplication.Run()
 
     def create_canvas(self):
         self.canvas_main = TCanvas(
@@ -18,27 +20,27 @@ class Canvas():
         self.canvas_main.Divide(2, 2)
         print('Canvas created.')
 
-    def create_histograms(self, freq_min, freq_max):
+    def create_histograms(self):
         self.freq_center = 0
         self.freq_tl = 243.2712156  # check what this value should be, prob should be variable
         self.nbins = int(1e2)  # placeholder value, should be variable
+        # setting normalised histogram range:
+        normalised_min = (self.freq_center + self.freq_min)/self.freq_tl
+        normalised_max = (self.freq_center + self.freq_max)/self.freq_tl
 
         # note: TH1F('name', 'title', int: nbinsx, xlow, xup)
-        
-        # simulated histogram? old name: hSim
-        h_sim = TH1F('h_sim', 'Simulated Something', int(200e3), 400, 700)
+
+        # hSim simulated histogram?
+        h_sim = TH1F('h_sim', 'Simulated Histogram', int(200e3), 400, 700)
         # hFFt_px_ref (reference?)
-        h_ref = TH1F('h_ref', 'Simulated', self.nbins,
-                     (self.freq_center + freq_min)/self.freq_tl,
-                     (self.freq_center+freq_max)/self.freq_tl)
+        h_ref = TH1F('h_ref', 'Simulated Reference', self.nbins,
+                     normalised_min, normalised_max)
         # hSRF Histogram of simulated revolution frequency
         h_simfreq = TH1F('h_simfreq', 'FFT_reference', self.nbins,
-                         (self.freq_center+freq_min)/self.freq_tl,
-                         (self.freq_center+freq_max)/self.freq_tl)
-        # hSRF Histogram of simulated revolution frequency
+                         normalised_min, normalised_max)
+        # hSRRF Histogram of simulated relative revolution frequency
         h_rel_simfreq = TH1F('h_rel_simfreq', 'Sim. Rev. Freq.', self.nbins,
-                             (self.freq_center+freq_min)/self.freq_tl,
-                             (self.freq_center+freq_max)/self.freq_tl)
+                             normalised_min, normalised_max)
 
         self.hist_list = [h_sim, h_ref, h_simfreq, h_rel_simfreq]
         print('histograms created')
@@ -47,10 +49,14 @@ class Canvas():
         # filling with data, placeholder method
         for i, histogram in enumerate(self.hist_list):
             self.canvas_main.cd(i+1)
-            histogram.FillRandom('gaus', 10000)
+            histogram.FillRandom('gaus', 1000)
             histogram.Draw()
+
+            # debugging - remove after
             print(f'filling histogram {i+1}')
-            
+            print(histogram)
+            print('         ---         ')
+
         self.canvas_main.Update()
         print('Histograms filled.')
 
@@ -58,7 +64,6 @@ class Canvas():
 def test():
     mycanvas = Canvas()
 
+
 if __name__ == '__main__':
     test()
-    # prevent canvas from closing in pyroot:
-    gApplication.Run()
