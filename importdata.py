f38d67cacecafdd344ca09297a395de9cdf48a83
gwgwhc <georgehudsonchang@gmail.com>
2021-11-22 14:24:30 +0100

    syntax changes

--- importdata.py
+++ importdata.py
@@ -8,35 +8,42 @@ import lisereader as lread
 from inputparams import*
 from scipy.optimize import minimize
 
+
 class ImportData():
-    def __init__(self,filename):
-        self.filename=filename
-        self.ring=Ring('ESR', 108.5)  # have to add more functionalities here
+    def __init__(self, filename):
+        self.filename = filename
+        self.ring = Ring('ESR', 108.5)  # have to add more functionalities here
         self._import()
 
     def _import(self):
         # import ame from barion:
-        ame=AMEData()
-        ame.init_ame_db
-        self.ame_data=ame.ame_table
+        self.ame = AMEData()
+        self.ame.init_ame_db
+        self.ame_data = self.ame.ame_table
+        
         # import input params
-        params_file='data/InputParameters.txt'
-        input_params=InputParams(params_file)
-        self.BRho=input_params.dict['Brho']
-        self.GammaT=input_params.dict['GAMMAT']
-        self.RefIso=input_params.dict['ReferenceIsotope']
-        self.RefQ=input_params.dict['ReferenceIsotopeCharge']
-        self.Harmonic=input_params.dict['Harmonic']
+        params_file = 'data/InputParameters.txt'
+        input_params = InputParams(params_file)
+        # after tominimize equation fixed, change to:
+        # pdict = input_params.dict
+        # or pdict = InputParams(params_file).dict
+        #but then will have to alter lisereader line.
+        
+        self.BRho = input_params.dict['Brho']
+        self.GammaT = input_params.dict['GAMMAT']
+        self.RefIso = input_params.dict['ReferenceIsotope']
+        self.RefQ = input_params.dict['ReferenceIsotopeCharge']
+        self.Harmonic = input_params.dict['Harmonic']
+        
         # Load LISE file
-        lise_file=lread.LISEreader(input_params.lisefile)
-        self.lise_data=lise_file.get_info_all()
-        self.m, self.moq, self.SRRF, self.SRF, self.Nx_SRF, self.Nx_SRRF = \
-            ([] for i in range(6))
+        lise_file = lread.LISEreader(input_params.lisefile)
+        self.lise_data = lise_file.get_info_all()
+        self.Nx_SRF, self.Nx_SRRF = ([] for i in range(2))
 
     def data(self):
-        LFRAMES=2**10
-        NFRAMES=2*4
-        iq=TIQData(self.filename)
+        LFRAMES = 2**10
+        NFRAMES = 2*4
+        iq = TIQData(self.filename)
         iq.read_samples(LFRAMES*NFRAMES)
 
         self.ff, self.pp, _ = iq.get_fft()  # 1D frec and power
@@ -45,48 +52,70 @@ class ImportData():
                       self.ff[0], iq.center + self.ff[-1])
 
         for i in range(len(self.ff)):
-            self.h.SetBinContent(i,self.pp[i])
-        self.nbins=self.h.GetXaxis().GetNbins()
-        self.frequence_min=self.h.GetXaxis().GetXmin()/1000+245
-        self.frequence_max=self.h.GetXaxis().GetXmax()/1000+245
-        self.y_max=self.h.GetMaximum()
+            self.h.SetBinContent(i, self.pp[i])
+        self.nbins = self.h.GetXaxis().GetNbins()
+        self.frequence_min = self.h.GetXaxis().GetXmin()/1000+245
+        self.frequence_max = self.h.GetXaxis().GetXmax()/1000+245
+        self.y_max = self.h.GetMaximum()
         self.h.GetXaxis().SetLimits(self.frequence_min, self.frequence_max)
 
-    def samples(self):        
+    def samples(self):
         for i, lise in enumerate(self.lise_data):
-            for ame in self.ame_data:
-                if lise[0]==ame[6] and lise[1]==ame[5]:
-                    particle_name=Particle(lise[2],lise[3],AMEData(),self.ring)
-                    self.m.append(AMEData.to_mev(particle_name.get_ionic_mass_in_u()))
-                    self.moq.append(particle_name.get_ionic_moq_in_u())
-                    if (str(lise[1])+lise[0]==self.RefIso and lise[4]==self.RefQ):
-                        self.aux=i
-                        moq_Rel=self.moq[i]
-                        self.gamma=sqrt(pow(self.BRho*self.RefQ*AMEData.CC/self.m[i],2)+1)
-                        self.beta=sqrt(self.gamma*self.gamma-1)/self.gamma
-                        self.velocity=AMEData.CC*self.beta
-                        self.Frequence_Rel=self.velocity/self.ring.circumference
-
-        for k in range(0, len(self.m)):
-            # 1. simulated relative revolution frequency
-            self.SRRF.append(1-1/self.GammaT/self.GammaT*(self.moq[k]-moq_Rel)/moq_Rel)
-            # 2. simulated revolution frequency
-            self.SRF.append(self.SRRF[k]*Frequence_Rel*self.Harmonic)
-
-        print('Brho inicial',self.BRho)
-        self.BRhoCorrection()
-        print('Brho final',self.BRho)
-        
-        for k in range(0, len(self.m)):#Calculate new simulated frecuency sample spectrum
-            self.SRF[k]=self.SRRF[k]*self.Frequence_Rel*self.Harmonic
+            #calculating mass and moq using ame and lise data together
+            m = [AMEData.to_mev(Particle(lise[2],lise[3],ame,self.ring).get_ionic_moq_in_u)
+                 for ame in self.ame_data if lise[0] == ame[6] and lise[1] == ame[5]]
+            moq = [Particle(lise[2],lise[3],ame,self.ring).get_ionic_moq_in_u
+                   for ame in self.ame_data if lise[0] == ame[6] and lise[1] == ame[5]]
             
-    def tominimize(self,x):#function to minimize (x=Brho); yup, it's big
-        tominimize=abs(self.ff[self.pp.argmax()]-((1-1/self.GammaT/self.GammaT*(self.moq[self.aux]-moq_Rel)/moq_Rel)*((AMEData.CC*(sqrt((sqrt(pow(x*self.RefQ*AMEData.CC/self.m[self.aux],2))*(sqrt(pow(x*self.RefQ*AMEData.CC/self.m[self.aux],2))-1)/(sqrt(pow(x*self.RefQ*AMEData.CC/self.m[self.aux],2))))/self.ring.circumference)*self.Harmonic))
-        return tominimize
+            # for ame in self.ame_data:
+            #     if lise[0] == ame[6] and lise[1] == ame[5]:
+            #         particle_name = Particle(
+            #             lise[2], lise[3], AMEData(), self.ring)
+            #         self.m.append(AMEData.to_mev(
+            #             particle_name.get_ionic_mass_in_u()))
+            #         self.moq.append(particle_name.get_ionic_moq_in_u())
                     
-    def BRhoCorrection(self):#Performs minimization of f_data[IsochroIon]-f_sample[RefIon(Brho)]      
+            # if reference particle, calculate variables
+            if (str(lise[1])+lise[0] == self.RefIso and lise[4] == self.RefQ):
+                self.aux = i
+                self.moq_Rel = self.moq[i]
+                self.gamma = sqrt(
+                    pow(self.BRho*self.RefQ*AMEData.CC/self.m[i], 2)+1)
+                self.beta = sqrt(self.gamma*self.gamma-1)/self.gamma
+                self.velocity = AMEData.CC*self.beta
+                self.Frequence_Rel = self.velocity/self.ring.circumference
+
+        # for k in range(0, len(self.m)):
+        #     # 1. simulated relative revolution frequency
+        #     self.SRRF.append(1-1/self.GammaT/self.GammaT *
+        #                      (self.moq[k]-self.moq_Rel)/self.moq_Rel)
+        #     # 2. simulated revolution frequency
+        #     self.SRF.append(self.SRRF[k]*self.Frequence_Rel*self.Harmonic)
+        
+        # simulated relative and non-rel revolution frequencies    
+        self.SRRF = [1-1/self.GammaT/self.GammaT*(self.moq[k]-self.moq_Rel)/self.moq_Rel
+                        for k, element in enumerate(m)]
+        self.SRF = [self.SRRF[k]*self.Frequence_Rel*self.Harmonic
+                    for k, element in enumerate(m)]
+        
+
+        print(f'Brho initial: {self.BRho}')
+        self.BRhoCorrection()
+        print(f'Brho final: {self.BRho}')
+
+        #why do you need this one?
+        for k in range(0, len(self.m)):  # Calculate new simulated frecuency sample spectrum
+            self.SRF[k] = self.SRRF[k]*self.Frequence_Rel*self.Harmonic
+
+    def tominimize(self, x):  # function to minimize (x=Brho); yup, it's big
+        tominimize = abs(self.ff[self.pp.argmax()]-((1-1/self.GammaT/self.GammaT*(self.moq[self.aux]-self.moq_Rel)/self.moq_Rel)*((AMEData.CC*(sqrt((sqrt(pow(x*self.RefQ*AMEData.CC/self.m[self.aux], 2))*(sqrt(pow(x*self.RefQ*AMEData.CC/self.m[self.aux], 2))-1)/(sqrt(pow(x*self.RefQ*AMEData.CC/self.m[self.aux], 2))))/self.ring.circumference)*self.Harmonic))
+        return tominimize
+
+    # Performs minimization of f_data[IsochroIon]-f_sample[RefIon(Brho)]
+    def BRhoCorrection(self):
         self.BRho=minimize(tominimize, [self.BRho])
-        self.gamma=sqrt(pow(self.BRho*self.RefQ*AMEData.CC/self.m[self.aux],2)+1)
+        self.gamma=sqrt(
+            pow(self.BRho*self.RefQ*AMEData.CC/self.m[self.aux], 2)+1)
         self.beta=sqrt(self.gamma*self.gamma-1)/self.gamma
         self.velocity=AMEData.CC*self.beta
         self.Frequence_Rel=self.velocity/self.ring.circumference
@@ -98,7 +127,7 @@ def main():
         for file in files:
             test=ImportData(file[:-1])  # initialization
             test.data()
-            test.samples() 
+            test.samples()
 
 if __name__ == '__main__':
     main()
--- pysimtof.py
+++ pysimtof.py
@@ -1,15 +1,19 @@
-import importdata, creategui, inputparams
+import creategui, inputparams
+# import importdata
 
 
 def main():
     parameter_filename = 'data/InputParameters.txt'
-    pdict=inputparams.InputParams(parameter_filename)
+    ip=inputparams.InputParams(parameter_filename)
     
-
-
-def test():
-    pass
+    # 'data/410-j' is now in pdict['']
+    print(ip.dict['rawdata_filename'])
+    
+    #now pass filename of rawdata to ImportData
+    
+    #then,
+    mycanvas = creategui.CreateGUI() 
 
 
 if __name__ == '__main__':
-    test()
+    main()
