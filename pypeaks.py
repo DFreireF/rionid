4de767472bf69035d3bdec4c1019da32aa834995
David Freire Fernández <D.FreireFernandez@gsi.de>
2021-11-25 21:15:18 +0100

    update this file

--- pypeaks.py
+++ pypeaks.py
@@ -1,92 +1,141 @@
 from ROOT import TCanvas,TMath,TH1,TH1F,TF1,TRandom,TSpectrum,TVirtualFitter 
 #fitting function compose by various Gaussians; change by Voigt function maybe
 from array import array
+from time import time
+import numpy as np
 
 def fpeaks(x,par):
     result = par[0] + par[1]*x[0] #line
-    for p in range(0,20): #for each peak 3 parameters
+    npeakstofit=(np.len(par))
+    print(npeakstofit,'?')
+    for p in range(0,npeakstofit):#for each peak 3 parameters
         norm  = par[3*p+2] 
         mean  = par[3*p+3]
         sigma = par[3*p+4]
         norm /= sigma*(TMath.Sqrt(TMath.TwoPi()))
         result += norm*TMath.Gaus(x[0],mean,sigma)
+        #print(sigma)
     return result
-    
+
 class FitPeaks():
 
     def __init__(self,npeaks,histogram):
         self.par = array('d',[])
         self.npeaks=npeaks
         self.histogram=histogram
+        self.h2=histogram
         self.peaks()
-
-    def peaks(self):
-        #Generate n peaks at random
+    #def __call__(self):
+        #self.peaks()
+        #self.fitting()
+        #self.c1.Update()
+        
+    def canvas(self):#Generates canvas, 1 with found peaks, 1 with fitting
+        self.c1 = TCanvas('c1','c1',10,10,1000,900)
+        self.c1.Divide(1,2)
+        
+    def generate_random_peaks(self):#Generate n peaks at random
         self.par.append(0.8)
         self.par.append(-0.6/1000)
-        h2=self.histogram
         gRandom=TRandom()
         for p in range(0,self.npeaks):
            self.par.append(1) # "height"
            self.par.append(10+gRandom.Rndm()*980)#[3*p+3] // "mean"
            self.par.append(3+2*gRandom.Rndm()) #[3*p+4] // "sigma"
-        #1D function          
-        c1 = TCanvas('c1','c1',10,10,1000,900)
-        c1.Divide(1,2)
-        c1.cd(1)
+           
+    def peaks(self):
+        #self.generate_random_peaks()
+        self.canvas()
+        self.c1.cd(1)
         self.histogram.Draw()
+        h2=self.histogram
         # Use TSpectrum to find the peak candidates
-        s = TSpectrum(2*self.npeaks) #(maximum number of peaks)
-        nfound = s.Search(self.histogram,2,"",0.10)
-        print(f'Found {nfound} candidate peaks to fit\n') 
+        self.s = TSpectrum(self.npeaks) #(maximum number of peaks)
+        self.nfound = self.s.Search(self.histogram,2,"",0.10)
+        print(f'Found {self.nfound} candidate peaks to fit\n')
+        
+        if self.npeaks<0: #if you dont want any fitting, just the peaks found
+            return None
+        
         # Estimate background using TSpectrum::Background
-        hb = s.Background(self.histogram,20,"same") #This function calculates the background spectrum in the input histogram, returned as a histogram.
+        hb = self.s.Background(self.histogram,20,"same") #This function calculates the background spectrum in the input histogram, returned as a histogram.
         if hb: #is there any other option than this? I mean, is it satisfied always?
-            c1.Update() 
-        if self.npeaks<0: #if you dont want any fitting
-            return 
+            self.c1.Update()  
         # estimate linear background using a fitting method
-        c1.cd(2) 
-        fline = TF1('fline','pol1',0,1000) #we will need to change the range 
-        self.histogram.Fit('fline','qn') 
-        # Loop on all found peaks. Eliminate peaks at the background level
-        self.par[0]=fline.GetParameter(0) 
-        self.par[1]=fline.GetParameter(1) 
-        #self.npeaks=0  #makes sense? 
-        xpeaks=s.GetPositionX() 
-        for p in range(0,nfound):
-              xp=xpeaks[p] 
-              bin=self.histogram.GetXaxis().FindBin(xp) 
-              yp=self.histogram.GetBinContent(bin) 
-              if (yp-TMath.Sqrt(yp) < fline.Eval(xp)):
-                  break
-              else:
-                  self.par[3*self.npeaks+2]=yp  #// "height"
-                  self.par[3*self.npeaks+3]=xp  #[3*npeaks+3]// "mean"
-                  self.par[3*self.npeaks+4]=3   #[3*npeaks+4]// "sigma"
-                  self.npeaks+=1
-                  
-        print(f'Found {self.npeaks} useful peaks to fit\n') 
-        print('Now fitting: Be patient\n')
-        #a=[]
-        #for i in range(0,1000):
-        #    a.append(self.fpeaks(i))
-        #[b.append(i) for i in range(0,1000)]
-        #a=self.fpeaks(b)
-        #a=self.fpeaks()
-        fit=TF1('fit',fpeaks,0,1000,2+3*self.npeaks)
+        self.c1.cd(2)
+        self.set_ranges()
+        #print(self.range_min)
+        self.fline = TF1('fline','pol1',self.range_min,self.range_max) #we will need to change the range 
+        self.histogram.Fit('fline','qn')
+        self.c1.Update()
+        #self.par[0]=self.fline.GetParameter(0)
+        #self.par[1]=self.fline.GetParameter(1)
+        self.par.append(self.fline.GetParameter(0)) #par[0] 
+        self.par.append(self.fline.GetParameter(1)) #par[1]
+        self.n_peakstofit()
+        start_time = time()
+        print(f'Now fitting: it takes some time \n')
+        #self.par=getpar.GetParameters()
+        npars=int(len(self.par))
+        #print(npars)
+        for i in range (0,10):
+            fit=TF1('fit',fpeaks,self.range_min,self.range_max,npars)
+            TVirtualFitter.Fitter(h2,10+npars-2)#*int(self.par[-1])
+            fit.SetParameters(self.par)
+            fit.SetNpx(1000)
+            h2.Fit(fit)
+            self.c1.Update()
+            getpar = self.h2.GetFunction('fit')
+            for j in range(getpar.GetNumberFreeParameters()):
+                self.par[j] = getpar.GetParameter(j)
+            print(self.par)
+            print(f"--{time()-start_time} seconds--")
+        #self.fitting()
+        
+    def n_peakstofit(self):# Loop on all found peaks. Eliminate peaks at the background level    
+        n_peakstofit=0
+        xpeaks=self.s.GetPositionX()
+        for p in range(0,self.nfound):
+            print(p)
+            xp=xpeaks[p]
+            bin=self.histogram.GetXaxis().FindBin(xp) 
+            yp=self.histogram.GetBinContent(bin)
+            #print(f'es {yp-TMath.Sqrt(yp)} > {self.fline.Eval(xp)} ??')
+            if (abs(yp-TMath.Sqrt(yp)) > self.fline.Eval(xp)):
+                self.par.append(yp)  #[3*n_peakstofit+2]=yp// "height"
+                self.par.append(xp)  #[3*n_peakstofit+3]=xp[3*npeaks+3]// "mean"
+                self.par.append(10)   #[3*n_peakstofit+4]=3#[3*npeaks+4]// "sigma"
+                n_peakstofit+=1
+                
+        #self.par.append(n_peakstofit)
+        #print(self.par)
+        print(f'Found {n_peakstofit} useful peaks to fit\n')
+        
+    def fitting(self):
+        #start_time = time()
+        #print(f'Now fitting: it takes some time \n')
+        #self.set_ranges()
+        #print(self.par[-1])
+        #input()
+        fit=TF1('fit',fpeaks,0,1000,2+3*int(self.par[-1]))
+        
         #fit.Draw()
-        #c1.Update()
-        #fit.Draw()
-        TVirtualFitter.Fitter(h2,10+3*self.npeaks)
+        #self.c1.Update()
+        input()
+        TVirtualFitter.Fitter(self.h2,10+3*int(self.par[-1]))
+        
         fit.SetParameters(self.par)
         fit.SetNpx(1000)
-        h2.Fit(fit)
-        #c1.Update()
+        self.h2.Fit(fit)
+        print(f"--{time()-start_time} seconds--")
+
+    def set_ranges(self):
+        self.range_min = self.histogram.GetXaxis().GetXmin()
+        self.range_max = self.histogram.GetXaxis().GetXmax()
+
       
 if __name__ == '__main__':
-  try:
-    #histo=....  
+  try:  
     FitPeaks(10,histo)
   except:
     raise
