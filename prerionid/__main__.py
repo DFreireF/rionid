d5deb095ca514c6c8765a09b3afd8ec5984a6871
DFreireF <D.FreireFernandez@gsi.de>
2024-05-11 21:33:30 +0200

    improving preprocessing + notebook for testing | sáb 11 may 2024 21:33:30 CEST

--- prerionid/__main__.py
+++ prerionid/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 import os
+from datetime import datetime
 import logging as log
 
 
@@ -16,43 +17,6 @@ def write_spectrum_to_csv(freq, power, filename, center = 0, out = None):
     print(f'created file: {file_name}')
     np.savetxt(file_name, final_data, header =
                f'Delta f [Hz] @ {center} [Hz]|Power [W]|Power [dBm]', delimiter = '|')
-          
-def main():
-    scriptname = 'RionID_DataProcessing' 
-    print(scriptname)
-    parser = argparse.ArgumentParser()
-
-    # Main argument
-    parser.add_argument('filename', type = str, nargs = '+', help = 'Name of the input file.')
-
-    # Arguments for processing the data
-    parser.add_argument('-t', '--time', type = float, nargs = '?', help = 'For how long in time to analyse.')
-    parser.add_argument('-s', '--skip', type = float, nargs = '?', help = 'Starting point in time of the analysis.')
-    parser.add_argument('-b', '--binning', type = int, nargs = '?', help = 'Number of frecuency bins.')
-
-    # Fancy arguments
-    parser.add_argument('-o', '--outdir', type = str, nargs = '?', default = os.getcwd(), help = 'Output directory.')
-    parser.add_argument('-v', '--verbose', help = 'Increase output verbosity', action = 'store_true')
-
-    # Actions
-    parser.add_argument('-fft', '--fft', help= 'Perform fft.', action = 'store_true')
-    
-    args = parser.parse_args()
-
-    if args.outdir:
-        if not os.path.isdir(args.outdir):
-            sys.exit('Output directory does not exist. Check it.')
-
-    print(f'Running {scriptname}. Processing...')
-    if args.verbose: log.basicConfig(level = log.DEBUG)
-
-    if ('txt') in args.filename[0]:
-        filename_list = read_masterfile(args.filename[0])
-        for file in filename_list:
-            create_exp_spectrum_csv(file[0], args.time, args.skip, args.binning, out = args.outdir, fft = args.fft)
-    else:
-        for file in args.filename:
-            create_exp_spectrum_csv(file, args.time, args.skip, args.binning, out = args.outdir, fft = args.fft) 
     
 def read_masterfile(master_filename):
     # reads list filenames with experiment data. [:-1] to remove eol sequence.
@@ -62,6 +26,47 @@ def create_exp_spectrum_csv(filename, time, skip, binning, out = None, fft = Non
     myexpdata = ProcessSchottkyData(filename, analysis_time = time, skip_time = skip, binning = binning, fft = fft)
     myexpdata._exp_data()
     write_spectrum_to_csv(myexpdata.frequency, myexpdata.power, filename, out = out)
+
+def create_exp_spectrum_npz(filename, time, skip, binning, out = None, fft = None):
+    myexpdata = ProcessSchottkyData(filename, analysis_time = time, skip_time = skip, binning = binning, fft = fft)
+    myexpdata.get_exp_data()
+    myexpdata.save_exp_data()
+
+def main():
+    scriptname = '(Pre)RionID' 
+    parser = argparse.ArgumentParser()
+
+    # Main argument
+    parser.add_argument('filename', type = str, nargs = '+', help = 'Name of the input file.')
+
+    # Arguments for processing the data
+    parser.add_argument('-t', '--time', type = float, nargs = '?', help = 'For how long in time to analyse.')
+    parser.add_argument('-s', '--skip', type = float, nargs = '?', help = 'Starting point in time of the analysis within the file.')
+    parser.add_argument('-b', '--binning', type = int, nargs = '?', help = 'Number of frecuency bins, e.g. 1024.')
+    parser.add_argument('-ts', '--timesize', type = float, nargs = '?', help = 'Size of the time bin in seconds, e.g. 1.4.')
+    parser.add_argument('-m', '--method', type=str, help= 'Perform fft via [npfft, fftw, welch, mtm]. E.g. five npfft', default= 'npfft')
+
+    # Fancy arguments
+    parser.add_argument('-o', '--outdir', type = str, nargs = '?', default = os.getcwd(), help = 'Output directory.')
+    parser.add_argument('-v', '--verbose', help = 'Increase output verbosity', action = 'store_true')
     
+    args = parser.parse_args()
+
+    if args.outdir:
+        if not os.path.isdir(args.outdir):
+            sys.exit('Output directory does not exist. Check it.')
+
+    print(f'Running {scriptname}. Processing...')
+    if args.verbose: 
+        log.basicConfig(level = log.DEBUG)
+
+    if ('txt') in args.filename[0]:
+        filename_list = read_masterfile(args.filename[0])
+        for file in filename_list:
+            create_exp_spectrum_npz(file[0], args.time, args.skip, args.binning, out = args.outdir, fft = args.fft)
+    else:
+        for file in args.filename:
+            create_exp_spectrum_npz(file, args.time, args.skip, args.binning, out = args.outdir, fft = args.fft) 
+
 if __name__ == '__main__':
     main()
--- prerionid/psdata.py
+++ prerionid/psdata.py
@@ -1,45 +1,49 @@
 import sys
 from iqtools import *
+from datetime import datetime
 
 class ProcessSchottkyData(object):
     '''
     Class for Schottky data processing
     '''
-    def __init__(self, filename, skip_time = None, analysis_time = None, binning = None, fft = False):
+    def __init__(self, filename, skip_time = None, analysis_time = None, binning = None, time_bin_size = None, method = 'npfft'):
 
         self.filename = filename
-        self.read_all = False
-        self.fft = fft
-        if skip_time is not None and analysis_time is not None and binning is not None: 
-            self.skip_time = skip_time
-            self.analysis_time = analysis_time
+        self.iq = get_iq_object(filename)
+        self.method = method #['npfft', 'fftw', 'welch', 'mtm']
+
+        if binning is not None:
             self.binning = binning
+        elif time_bin_size is not None:
+            self.binning = int(self.iq.fs * time_bin_size)
+
+        if skip_time is not None and analysis_time is not None:
+            self.nframes = int(analysis_time * self.iq.fs / self.binning)
+            self.sframes = int(skip_time     * self.iq.fs / self.binning)
         else:
             self.read_all = True
-        
-    def root_data(self):
-        
-        from ROOT import TFile
-        fdata = TFile(self.filename)
-        histogram = fdata.Get(fdata.GetListOfKeys()[0].GetName())
-        freq = np.array([[histogram.GetXaxis().GetBinCenter(i) * 1e6] for i in range(1, histogram.GetNbinsX())]) # 1e6 for units
-        power = np.array([[histogram.GetBinContent(i)] for i in range(1, histogram.GetNbinsX())])          
-        return freq, power
 
-    def specan_data(self):
-        
-        freq, power, _ = read_rsa_specan_xml(self.filename)
-        power = power - power.min() #in order to avoid negative values: power - (-|value_min|) > power
-        normalized_power = power / power.max()
-        return freq, normalized_power
-
-    def iq_data(self, read_all = False):
+    def tdms_data(self, read_all = False):
 
         iq = get_iq_object(self.filename)
-        iq.read_samples(1) # Necessary for iq.tdms files
         if not read_all:
             nframes = int(self.analysis_time * iq.fs / self.binning)
             sframes = int(self.skip_time * iq.fs / self.binning)
+            iq.read(nframes = nframes, lframes = self.binning, sframes = sframes)
+        else:
+            iq.read_samples(iq.nsamples_total)
+        
+        xx, _, zz = iq.get_spectrogram(nframes = nframes, lframes = self.binning)
+        
+        freq = xx[0,:] + iq.center
+        power = (azz[0, :]).reshape(len(azz[0, :]), 1) #power
+        return freq, power
+    
+    def tiq_data(self, time = None, skip = None):
+        
+        if time and skip:
+            nframes = int(self.analysis_time * self.iq.fs / self.binning)
+            sframes = int(self.skip_time * self.iq.fs / self.binning)
             iq.read(nframes = nframes, lframes = self.binning, sframes = sframes)
         else:
             iq.read_samples(iq.nsamples_total)
@@ -54,14 +58,32 @@ class ProcessSchottkyData(object):
             power = (azz[0, :]).reshape(len(azz[0, :]), 1) #power
         normalized_power = power / power.max()
         return freq, normalized_power
-    
-    def _exp_data(self, fft = False):
-        
-        if '.root' in self.filename: 
-            self.frequency, self.power = self.root_data()
-        elif '.Specan' in self.filename: 
-            self.frequency, self.power = self.specan_data()        
-        elif 'iq' in self.filename: 
-            self.frequency, self.power = self.iq_data(read_all = self.read_all) # tiq and iq.tdms ; also the order is important
-        else: sys.exit()
 
+    def get_exp_data(self):
+        if 'tdms' in self.filename: 
+            self.frequency, self.power = self.tdms_data()
+        elif 'tiq' in self.filename: 
+            self.frequency, self.power = self.tiq_data()        
+        else: 
+            sys.exit()
+
+    def save_exp_data(self):
+        #add also path
+        outfile = f'{self.filename}_{datetime.now().strftime('%d-%H.%M.%S')}'
+        np.savez(outfile, x = self.frequency, y = self.power)
+
+#def root_data(self):
+#        
+#    from ROOT import TFile
+#    fdata = TFile(self.filename)
+#    histogram = fdata.Get(fdata.GetListOfKeys()[0].GetName())
+#    freq = np.array([[histogram.GetXaxis().GetBinCenter(i) * 1e6] for i in range(1,histogram.#GetNbinsX())]) # 1e6 for units
+#    power = np.array([[histogram.GetBinContent(i)] for i in range(1, histogram.GetNbins#())])          
+#    return freq, power
+#
+#def specan_data(self):
+#        
+#    freq, power, _ = read_rsa_specan_xml(self.filename)
+#    power = power - power.min() #in order to avoid negative values: power - (-|value_min|) > #power
+#    normalized_power = power / power.max()
+#    return freq, normalized_power
