51900cb830d9072fdefa00419fb3897d8df0786c
David Freire Fernandez <freire@pn2408475.lanl.gov>
2025-03-25 17:18:35 -0600

    publishing workflow Tue Mar 25 17:18:35 MDT 2025

---
+++ installation.sh
@@ -0,0 +1,4 @@
+pip install git+https://github.com/xaratustrah/barion.git
+pip install git+https://github.com/xaratustrah/iqtools.git
+pip install git+https://github.com/gwgwhc/lisereader.git
+pip install RionID
